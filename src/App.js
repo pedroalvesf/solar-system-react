e947eed37587ca288557a33338d02dd20075fe2e
Ellen Santos <ellencssp@gmail.com>
2022-08-21 21:31:11 -0300

    Create component Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-      <h2>Oie</h2>
+      <Header />
       </div>
     )
   }
